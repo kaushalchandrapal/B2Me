95fd15b1e472f62ed495c381b83b787f07236cb2
Kaushal Chandrapal <kaushalchandrapal@Kaushals-MacBook-Pro.local>
2021-04-06 15:14:33 +0530

    github check

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,18 @@
-
+import React from 'react'
+import { Container } from 'react-bootstrap'
+import Footer from "./Components/Footer"
+import Header from "./Components/Header"
+import HomeScreen from './Screens/HomeScreen'
 const App = () => {
   return (
     <>
-      <h1>Welcome to B2Me</h1>
+      <Header/>
+      <main className='py-3'>
+        <Container>
+          <HomeScreen />
+        </Container>
+      </main>
+      <Footer/>
     </>
   );
 }
